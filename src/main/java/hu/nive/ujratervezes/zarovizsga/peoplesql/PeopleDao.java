86b7e213bb059b66715c5798fa7822bc1de9e2c0
juhaszg13 <66171665+juhaszg13@users.noreply.github.com>
2021-03-22 19:05:18 +0100

    PeopleDao

---
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/peoplesql/PeopleDao.java
@@ -0,0 +1,41 @@
+package hu.nive.ujratervezes.zarovizsga.peoplesql;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PeopleDao {
+    private DataSource dataSource;
+
+    public PeopleDao(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public String findIpByName(String firstName, String lastName) {
+        try (
+                Connection conn = dataSource.getConnection();
+                PreparedStatement stmt =
+                        conn.prepareStatement("select ip_address from people where first_name = ? and last_name=?");
+        ) {
+            stmt.setString(1, firstName);
+            stmt.setString(2, lastName);
+
+            try (
+                    ResultSet rs = stmt.executeQuery();
+            ) {
+                if (rs.next()) {
+                    String ip = rs.getString("ip_address");
+                    return ip;
+                }
+                throw new IllegalArgumentException("No result");
+            } catch (SQLException sqle) {
+                throw new IllegalArgumentException("Error by insert", sqle);
+            }
+        } catch (SQLException sqle) {
+            throw new IllegalArgumentException("Error by select", sqle);
+        }
+
+
+    }
+}
